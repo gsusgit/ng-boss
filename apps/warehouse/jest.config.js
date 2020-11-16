dbb205bb5238f137eb18f3e06016fe6fee759c19
Jesus Fernandez <fernandezjodar.jesus@gmail.com>
2020-11-16 11:59:21 +0100

    Created new basic Nx Project

---
+++ apps/warehouse/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  displayName: 'warehouse',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
+  coverageDirectory: '../../coverage/apps/warehouse',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/shop',
+    '<rootDir>/apps/warehouse',
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/shared/domain',
+    '<rootDir>/libs/shared/ui',
+  ],
+};
