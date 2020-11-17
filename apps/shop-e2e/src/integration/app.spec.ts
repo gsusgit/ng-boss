bb7adc15d51f01a68af176ce476d057f8a247a12
Jesus Fernandez <fernandezjodar.jesus@gmail.com>
2020-11-17 10:35:27 +0100

    Added Cypress and Jest tests

--- apps/shop-e2e/src/integration/app.spec.ts
+++ apps/shop-e2e/src/integration/app.spec.ts
@@ -1,13 +1,9 @@
-import { getGreeting } from '../support/app.po';
-
-describe('shop', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to shop!');
+import { getGreeting, visitHome } from '../support/app.po';
+describe('GIVEN: the shop web app', () => {
+  beforeEach(() => visitHome());
+  context('WHEN: user visits home page', () => {
+    it('THEN: should display welcome message', () => {
+      getGreeting().contains('Hello world');
+    });
   });
 });
